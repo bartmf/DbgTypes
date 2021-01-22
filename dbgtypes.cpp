eaf65f6fb27f9b5134d98c5c4f719249d065ecae
Bart_MF <bartcsc1278@gmail.com>
2021-01-22 10:51:00 +0300

    add files

---
+++ dbgtypes.cpp
@@ -0,0 +1,5 @@
+#include "dbgtypes.h"
+
+DbgTypes::DbgTypes()
+{
+}
---
+++ dbgtypes.h
@@ -0,0 +1,10 @@
+#ifndef DBGTYPES_H
+#define DBGTYPES_H
+
+class DbgTypes
+{
+public:
+    DbgTypes();
+};
+
+#endif // DBGTYPES_H
